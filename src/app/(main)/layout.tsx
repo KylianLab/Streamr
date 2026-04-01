5747d65ee055fc6a041d83232d17fe1a39ea6ff7
Update <bernath.kylian.pro@gmail.com>
2026-04-01 21:12:44 +0200

    Update layout.tsx

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,11 @@
+import { Suspense } from "react";
 import { Navbar } from "@/components/layout/navbar";
 import { ToastContainer } from "@/components/ui/toast";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Navbar />
+      <Suspense><Navbar /></Suspense>
       <main className="min-h-screen pt-16">{children}</main>
       <ToastContainer />
     </>
